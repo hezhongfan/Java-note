8f5bda629f1b1430b8fa9cfad3de1d0a883650f9
hezhongfan <hezhongfan@qingzu.com.cn>
2020-03-07 23:45:57 +0800

    demo init

---
+++ src/main/java/com/hzf/hashMapTest/HashMapTest.java
@@ -0,0 +1,10 @@
+package com.hzf.hashMapTest;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+    }
+}
